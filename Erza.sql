4c20d65be6356b375e84fb18e8c92ef114dd0ba9
macsimbelous <macsimbelous@gmail.com>
2015-04-20 11:22:04 +0800

    SQL 2

--- Erza.sql
+++ Erza.sql
@@ -60,3 +60,58 @@ WITH (
 );
 ALTER TABLE image_tags
   OWNER TO "Erza";
+
+-- Function: add_tag(character varying)
+
+-- DROP FUNCTION add_tag(character varying);
+
+CREATE OR REPLACE FUNCTION add_tag(par1 character varying)
+  RETURNS bigint AS
+$BODY$
+DECLARE
+    res1 bigint;
+BEGIN
+
+SELECT tag_id INTO res1 FROM   tags WHERE  tag = $1;
+
+IF NOT FOUND THEN
+   insert into tags (tag) values ($1);
+   select tag_id INTO res1 from tags where tag = $1;
+END IF;
+
+RETURN res1;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION add_tag(character varying)
+  OWNER TO "Erza";
+
+-- Function: add_image(boolean, character varying, character varying)
+
+-- DROP FUNCTION add_image(boolean, character varying, character varying);
+
+CREATE OR REPLACE FUNCTION add_image(
+    is_deleted boolean,
+    hash character varying,
+    file_path character varying)
+  RETURNS bigint AS
+$BODY$
+DECLARE
+    res1 bigint;
+BEGIN
+
+SELECT image_id INTO res1 FROM images WHERE hash = $2;
+
+IF NOT FOUND THEN
+   INSERT INTO images (is_deleted, hash, file_path) VALUES ($1, $2, $3);
+   SELECT image_id INTO res1 FROM images WHERE hash = $2;
+END IF;
+
+RETURN res1;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION add_image(boolean, character varying, character varying)
+  OWNER TO "Erza";
